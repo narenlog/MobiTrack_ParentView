f564da1c6fa12f42d95a37215fdd85a59bcb7d31
Narender Loganathan <narenlog@gmail.com>
2013-11-25 00:17:33 +0530

    Added user auth via devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Locationview::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Required for Devise
+  config.action_mailer.default_url_options = { :host => 'http://lovedoneslocation.herokuapp.com:3000' }
 end
